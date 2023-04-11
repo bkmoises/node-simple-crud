98f8ae6f1daf32075ca93304ee93aeccad051eb3
bkmoises <bk_moises@hotmail.com>
2023-04-11 16:10:56 -0300

    configurado os decorations

--- simple-crud/src/entity/User.ts
+++ simple-crud/src/entity/User.ts
@@ -3,16 +3,13 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 @Entity()
 export class User {
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number
 
-    @Column()
-    firstName: string
+  @Column()
+  name: string
 
-    @Column()
-    lastName: string
-
-    @Column()
-    age: number
+  @Column()
+  email: string
 
 }
